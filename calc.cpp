5c9777575a5786f18f1cbf2760dc61d177757ed6
bogdan <ilenko0bogdan@gmail.com>
2023-09-29 12:46:32 +0300

    New feature added

---
+++ calc.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+  double a, b, x;
+  cout <<"Введіть число a і b: "; cin >> a >> b;
+  x=pow(a,b);
+  cout <<"Результат: " << x << endl;
+  return 0;
+}
